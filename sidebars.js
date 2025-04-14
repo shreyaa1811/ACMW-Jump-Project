49e7edf8c0755b3ca45a2822689d7a490ad7e1ef
shreyaa1811 <f20230300@dubai.bits-pilani.ac.in>
2025-04-14 17:52:12 +0400

    fixed error in doc file name

--- sidebars.js
+++ sidebars.js
@@ -32,7 +32,7 @@ const sidebars = {
        type: 'category',
        label: 'Project Guides',
        items: [
-         'documentation/db-installation',
+         'documentation/db-installations',
          'documentation/datagrip'
        ],
      },
